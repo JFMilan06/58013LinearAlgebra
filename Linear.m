8da0b7d8c58c40f4138e255973bec7f3c290f198
John Frederick L. Milan <123443594+JFMilan06@users.noreply.github.com>
2023-10-02 09:35:56 +0800

    Add files via upload

--- Linear.m
+++ Linear.m
@@ -11,3 +11,12 @@ x=det(A1)+det(A2)+det(A3)
 x1 = det(A1/A)
 x2 = det(A2/A)
 x3 = det(A3/A)
+
+syms x y z;
+eq1 = x+y+z ==5;
+eq2 = x+2*y+2*z==6;
+eq3 = x+2*y+3*z==8;
+sol = solve([eq1,eq2,eq3],[x,y,z])
+xsol = sol.x
+ysol=sol.y
+zsol=sol.z
